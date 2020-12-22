d77d3fb4b59bfe9aacc09a2a061cc3879ac15db6
Davitavyan <44241530+Davitavyan@users.noreply.github.com>
2020-12-23 00:32:58 +0400

    Update subproc.py

--- modul/subproc.py
+++ modul/subproc.py
@@ -1,6 +1,5 @@
 import subprocess
-#result = subprocess.run(['ls', '-a', '/'], stdout=subprocess.PIPE)
-result = subprocess.run(['pwd'], stdout=subprocess.PIPE)
+result = subprocess.run(['ls', '-a', '/'], stdout=subprocess.PIPE)
 print(result.stdout.decode('utf-8'))
 print(result.stderr)
 
